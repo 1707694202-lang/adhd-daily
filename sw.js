ae32275bd7c5765306995602dd299aa5bb131943
1707694202-lang <1707694202@qq.com>
2026-09-10 03:14:24 +0800

    feat: new theme cream garden (#EDEDE4/#B8CEC0/#F2D8E0) (v35) (update sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 离线缓存：让网页装到主屏后，没网也能打开（就像真装的软件）
    每次改版本号就能让旧缓存自动失效 */
-var CACHE = 'adhd-v34';
+var CACHE = 'adhd-v35';
 
 var ASSETS = ['./', './index.html', './icon.png', './manifest.webmanifest'];
 
